574cad100db35431d0ddc4e2d54339e83fd3757f
Dusan Orlovic <dusan.orlovic@trk-inovacije.com>
2014-03-03 01:51:46 +0100

    adding awk

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-
 #check for curl
 #check if password is filled
 #check if publish url starts with ftp
@@ -17,3 +16,10 @@ curl -u $FTP_USERNAME:$FTP_PASSWORD $FTP_URL/
 echo "find . -type f -exec curl -u $FTP_USERNAME:FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;"
 
 #find . -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;
+
+awk 'BEGIN {}
+$1~/M|D/ { print "removing " $2;system("curl -u $FTP_USERNAME:$FTP_PASSWORD -X \"DELE "$2"\" $FTP_URL/ ") }
+$1~/M|A/ { print "adding " $2; system("curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T "$2" $FTP_URL/"$2) }
+
+END {} ' $FTP_DIFF_FILE
+
