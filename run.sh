7dac77d10ebb11e7a0b7c17b99c11d842aaf5a44
Dusan Orlovic <dusan.orlovic@trk-inovacije.com>
2014-03-05 04:48:48 +0100

    version

--- run.sh
+++ run.sh
@@ -14,15 +14,11 @@ then
     fail "missing option \"password\", aborting"
 fi
 
-# export is needed because awk is calling system subprocess
-export DESTINATION=$WERCKER_FTP_DEPLOY_DESTINATION
-export USERNAME=$WERCKER_FTP_DEPLOY_USERNAME
-export PASSWORD=$WERCKER_FTP_DEPLOY_PASSWORD
-export REMOTE_FILE=$WERCKER_FTP_DEPLOY_REMOTE_FILE
-
-# pwd is /pipeline/build
-# $WERCKER_BUILD is /home/ubuntu
-# $WERCKER_OUTPUT is /home/ubuntu
+DESTINATION=$WERCKER_FTP_DEPLOY_DESTINATION
+USERNAME=$WERCKER_FTP_DEPLOY_USERNAME
+PASSWORD=$WERCKER_FTP_DEPLOY_PASSWORD
+REMOTE_FILE=$WERCKER_FTP_DEPLOY_REMOTE_FILE
+TIMEOUT=3
 
 if [ ! -n "$WERCKER_FTP_DEPLOY_REMOTE_FILE" ]
 then
@@ -41,17 +37,27 @@ find . -type f -exec md5sum {} > $WERCKER_CACHE_DIR/local.txt \;
 
 curl -u $USERNAME:$PASSWORD  $DESTINATION/remote.txt -o $WERCKER_CACHE_DIR/remote.txt || (echo "no remote.txt file" && touch $WERCKER_CACHE_DIR/remote.txt )
 
+
 diff $WERCKER_CACHE_DIR/local.txt $WERCKER_CACHE_DIR/remote.txt | awk '{print $3}' | sort -u > $WERCKER_CACHE_DIR/diff.txt
 
 echo "start removing and push new or changed files"
+date_start=$(date +"%s")
 while read file_name; do
   if [  -n "$file_name" ];
   then
     echo $file_name
     curl -u $USERNAME:$PASSWORD -X "DELE $file_name" $DESTINATION/ || echo "$file_name does not exists on server"
+    sed -i "/$file_name/d" $WERCKER_CACHE_DIR/remote.txt 
     if [ -f $file_name ];
     then
       curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name"
+      md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
+    fi
+    if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
+    then
+      echo "to much time $(( ($(date +"%s") - $date_start) / 60 )), pushing uploaded hesh"
+      mv $WERCKER_CACHE_DIR/remote.txt $WERCKER_CACHE_DIR/local.txt
+      break
     fi
   fi
 done < $WERCKER_CACHE_DIR/diff.txt
