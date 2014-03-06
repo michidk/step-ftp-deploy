e31f1a2895ab28a70fa1d03d0c91b4646475c55d
Dusan Orlovic <dusan.orlovic@trk-inovacije.com>
2014-03-06 15:08:51 +0100

    adding, changing, removing

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
+# TODO is destination url is not create, maybe we should create it
+# 
+
+# curl 19 response
+# 250 OK deleted
+# 550 missing file
+
+# removing is done with -Q "-DELE $file_name" src: http://curl.haxx.se/mail/archive-2009-01/0040.html
+
+
 # confirm environment variables
 if [ ! -n "$WERCKER_FTP_DEPLOY_DESTINATION" ]
 then
@@ -21,7 +31,7 @@ REMOTE_FILE=$WERCKER_FTP_DEPLOY_REMOTE_FILE
 
 if [ ! -n "$WERCKER_FTP_DEPLOY_REMOTE_FILE" ]
 then
-    echo "missing option \"remote-file\" so we will use all files"
+    debug "missing option \"remote-file\" so we will use remote.txt"
     REMOTE_FILE=remote.txt
 fi
 
@@ -33,66 +43,85 @@ if [  -n "$WERCKER_FTP_DEPLOY_TIMEOUT" ]
 then
     TIMEOUT=$WERCKER_FTP_DEPLOY_TIMEOUT
 fi
-echo "TIMEOUT is set to $TIMEOUT min. If wercker stops this script before TIMEOUT then $REMOTE_FILE lose synchronization. Clean FTP destination and start again."
+debug "TIMEOUT is set to $TIMEOUT min. If wercker stops this script before TIMEOUT then $REMOTE_FILE lose synchronization. Clean FTP destination and start again."
 
-echo "Test connection and list $DESTINATION files"
+debug "Test connection and list $DESTINATION files"
 echo "curl -u $USERNAME:do_not_show_PASSWORD_in_log $DESTINATION/"
 curl -u $USERNAME:$PASSWORD $DESTINATION/
 
-echo "Calculating md5sum for local files" 
+debug "Calculating md5sum for local files" 
 find . -type f -exec md5sum {} > $WERCKER_CACHE_DIR/local.txt \;
 sort -k 2 -u $WERCKER_CACHE_DIR/local.txt -o $WERCKER_CACHE_DIR/local.txt > /dev/null
 
-echo "Obtaining $REMOTE_FILE"
-curl -u $USERNAME:$PASSWORD  $DESTINATION/$REMOTE_FILE -o $WERCKER_CACHE_DIR/remote.txt || (echo "No $REMOTE_FILE file" && echo "" > $WERCKER_CACHE_DIR/remote.txt )
-echo "Sort unique"
+debug "Obtaining $REMOTE_FILE"
+curl -u $USERNAME:$PASSWORD  $DESTINATION/$REMOTE_FILE -o $WERCKER_CACHE_DIR/remote.txt || (debug "No $REMOTE_FILE file" && echo "" > $WERCKER_CACHE_DIR/remote.txt )
 sort -k 2 -u $WERCKER_CACHE_DIR/remote.txt -o $WERCKER_CACHE_DIR/remote.txt > /dev/null
 
-echo "Sort all differences"
-diff $WERCKER_CACHE_DIR/local.txt $WERCKER_CACHE_DIR/remote.txt | awk '{print $3}' | sort -u > $WERCKER_CACHE_DIR/diff.txt 
-echo "total number of changed files "
-cat $WERCKER_CACHE_DIR/diff.txt | wc -l
+debug "Find new files "
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='' --new-line-format='%l
+' --unchanged-line-format=''  <(cut -d' ' -f3 $WERCKER_CACHE_DIR/remote.txt) <(cut -d' ' -f3 $WERCKER_CACHE_DIR/local.txt) > $WERCKER_CACHE_DIR/new.txt
+# remove empty lines
+sed -i '/^$/d' $WERCKER_CACHE_DIR/new.txt
+wc -l < $WERCKER_CACHE_DIR/new.txt
 
-echo "Start removing and push new or changed files"
-# if file is in diff.txt that means it is changed, removed of added
-# in all cases it should be removed from server
-# if it exists on local, then it should be pushed
+debug "Find removed files"
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='%l
+' --new-line-format='' --unchanged-line-format=''  <(cut -d' ' -f3 $WERCKER_CACHE_DIR/remote.txt) <(cut -d' ' -f3 $WERCKER_CACHE_DIR/local.txt) > $WERCKER_CACHE_DIR/removed.txt
+sed -i '/^$/d' $WERCKER_CACHE_DIR/removed.txt
+wc -l < $WERCKER_CACHE_DIR/removed.txt
+
+debug "Find changed files"
+grep -v -f $WERCKER_CACHE_DIR/new.txt $WERCKER_CACHE_DIR/local.txt > $WERCKER_CACHE_DIR/same_local.txt
+grep -v -f $WERCKER_CACHE_DIR/removed.txt $WERCKER_CACHE_DIR/remote.txt > $WERCKER_CACHE_DIR/same_remote.txt
+diff --ignore-case -b --ignore-blank-lines  --old-line-format='' --new-line-format='
+%l' --unchanged-line-format=''  $WERCKER_CACHE_DIR/same_remote.txt $WERCKER_CACHE_DIR/same_local.txt | awk '{print $2}' > $WERCKER_CACHE_DIR/changed.txt
+sed -i '/^$/d' $WERCKER_CACHE_DIR/changed.txt
+wc -l < $WERCKER_CACHE_DIR/changed.txt
+
+
+debug "Start uploading new files"
 while read file_name; do
-  if [  -n "$file_name" ];
+  if [ !  -n "$file_name" ];
   then
+    fail "$file_name should exists"
+  else
     echo $file_name
-    curl -u $USERNAME:$PASSWORD -X "DELE $file_name" $DESTINATION/ || echo "$file_name does not exists on server"
-    # remove it from remote list also.
-    # it does not change anything if file were not there
-    sed -i "\|\b$file_name\b|d" $WERCKER_CACHE_DIR/remote.txt 
-    if [ -f $file_name ];
-    then
-      # it is on local, so push it to server
-      curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name" || echo "failed to push $file_name!!!!! do not know what to do. please remove all files from $DESTINATION and start again"
-      md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
-    fi
-    if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
-    then
-      echo "TIMEOUT $TIMEOUT min has expired, pushing $REMOTE_FILE before wercker stop the script."
-      curl -u $USERNAME:$PASSWORD -X "DELE $REMOTE_FILE" $DESTINATION/ || echo "$REMOTE_FILE has not been exist at the server"
-      curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE"
-
-      fail "Please run again this script to finish all your files."
-    fi
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name" || fail "failed to push $file_name!!!!! do not know what to do. please remove all files from $DESTINATION and start again"
+    md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
   fi
-done < $WERCKER_CACHE_DIR/diff.txt
+  if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
+  then
+    fail "TIMEOUT $TIMEOUT min has expired. Please run again this script to finish all your files."
+  fi
+done < $WERCKER_CACHE_DIR/new.txt
 
-# local and remote should be equal
-sort -k 2 -u $WERCKER_CACHE_DIR/remote.txt -o $WERCKER_CACHE_DIR/remote.txt > /dev/null
-if diff $WERCKER_CACHE_DIR/remote.txt $WERCKER_CACHE_DIR/local.txt > /dev/null;then
-  echo "ok"
-else
-  echo "They should not be different. Please remove all files from $DESTINATION and start Again"
-fi
+debug "Start uploading changed files"
+while read file_name; do
+  if [ !  -n "$file_name" ];
+  then
+    fail "$file_name should exists"
+  else
+    echo $file_name
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$file_name" "$DESTINATION/$file_name" || fail "failed to push $file_name!!!!! do not know what to do. please remove all files from $DESTINATION and start again"
+    sed -i "\|\b$file_name\b|d" $WERCKER_CACHE_DIR/remote.txt 
+    md5sum $file_name >> $WERCKER_CACHE_DIR/remote.txt
+    curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
+  fi
+  if [ "$TIMEOUT" -le $(( ($(date +"%s") - $date_start) / 60 )) ];
+  then
+    fail "TIMEOUT $TIMEOUT min has expired. Please run again this script to finish all your files."
+  fi
+done < $WERCKER_CACHE_DIR/changed.txt
 
-curl -u $USERNAME:$PASSWORD -X "DELE $REMOTE_FILE" $DESTINATION/ || echo "$REMOTE_FILE did not exists on server"
-echo "Uploading $REMOTE_FILE"
-curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/local.txt" "$DESTINATION/$REMOTE_FILE"
+debug "Start removing files"
+while read file_name; do
+  echo $file_name
+  curl -u $USERNAME:$PASSWORD -Q "-DELE $file_name" $DESTINATION/ > /dev/null || fail "$file_name does not exists on server. Please change or remote your $REMOTE_FILE"
+  sed -i "\|\b$file_name\b|d" $WERCKER_CACHE_DIR/remote.txt 
+  curl -u $USERNAME:$PASSWORD --ftp-create-dirs -T "$WERCKER_CACHE_DIR/remote.txt" "$DESTINATION/$REMOTE_FILE" || fail "failed to push $REMOTE_FILE. It is not in sync anymore. Please remove all files from $DESTINATION and start again"
+done < $WERCKER_CACHE_DIR/removed.txt
 
-echo "Done uploading"
+success "Done."
+
 
